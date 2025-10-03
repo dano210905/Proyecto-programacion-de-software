917ede6a0b301841044b1f19b566c7fa9a362bde
dano210905 <dany.hurtac@gmail.com>
2025-10-03 18:49:28 -0500

    agregacion de entidades

---
+++ Proyectopsoft/library/Empleado.cs
@@ -0,0 +1,13 @@
+﻿namespace library
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+        public string AreaTrabajo { get; set; }
+        public string MetasBienestar { get; set; }
+        public string RestriccionesMedicas { get; set; }
+        public string Disponibilidad { get; set; }
+    }
+}
---
+++ Proyectopsoft/library/PBienestar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    internal class PBienestar
+    {
+        public int IdPrograma { get; set; }
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+        public string Duracion { get; set; }
+        public string Modalidad { get; set; }
+        public string Frecuencia { get; set; }
+
+    }
+}
