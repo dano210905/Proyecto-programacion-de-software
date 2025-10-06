77c87a373a15ceae5f32e8af51c0caa11bb1f5e6
dano210905 <dany.hurtac@gmail.com>
2025-10-06 12:50:40 -0500

    Controllers

---
+++ Proyectopsoft/apiproyect/Controllers/EmpleadoController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using apiproyect.Data;
+using Microsoft.EntityFrameworkCore;
+using library;
+
+
+namespace apiproyect.Controllers
+{
+    public class EmpleadoController : ControllerBase
+    {
+        private readonly Datacontext _context;
+
+        public EmpleadoController(Datacontext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id:int }")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var Empleado = await _context.Empleados
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (Empleado == null)
+            {
+                return NotFound("El empleado no fue encontrado");
+            }
+            return Ok(Empleado);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Empleado empleado)
+        {
+
+            _context.Empleados.Add(empleado);
+            await _context.SaveChangesAsync();
+            return Ok(empleado);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> put(Empleado empleado)
+        {
+
+            _context.Empleados.Add(empleado);
+            await _context.SaveChangesAsync();
+            return Ok(empleado);
+        }
+
+        [HttpDelete("{id:int }")]
+        public async Task<IActionResult>  Delete(int id)
+        {
+            var Fafect = await _context.Empleados
+                .Where(x => x.Id == id).ExecuteDeleteAsync();
+            if (Fafect==0)
+            {
+                return NotFound();   
+            }
+            return NoContent(); 
+
+
+        }
+
+
+
+    }
+}
---
+++ Proyectopsoft/apiproyect/Controllers/EspecialistaController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using apiproyect.Data;
+using Microsoft.EntityFrameworkCore;
+using library;
+
+
+namespace apiproyect.Controllers
+{
+    public class EspecialistaController : ControllerBase
+    {
+        private readonly Datacontext _context;
+
+        public EspecialistaController(Datacontext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id:int }")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var Especialista = await _context.Empleados
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (Especialista == null)
+            {
+                return NotFound("El empleado no fue encontrado");
+            }
+            return Ok(Especialista);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Especialista especialista)
+        {
+
+            _context.Especialistas.Add(especialista);
+            await _context.SaveChangesAsync();
+            return Ok(especialista);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> put(Especialista especialista)
+        {
+
+            _context.Especialistas.Add(especialista);
+            await _context.SaveChangesAsync();
+            return Ok(especialista);
+        }
+
+        [HttpDelete("{id:int }")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var Fafect = await _context.Especialistas
+                .Where(x => x.Id == id).ExecuteDeleteAsync();
+            if (Fafect == 0)
+            {
+                return NotFound();
+            }
+            return NoContent();
+
+
+        }
+
+
+
+    }
+}
